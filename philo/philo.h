0e4fdf904a9b2e5336c25ef054d71511559ce2a5
tinitiuset <tinitiuset@gmail.com>
2023-04-02 20:49:56 +0200

    Added printable states

--- philo/philo.h
+++ philo/philo.h
@@ -6,11 +6,19 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 19:51:38 by mvalient          #+#    #+#             */
-/*   Updated: 2023/04/02 19:53:13 by mvalient         ###   ########.fr       */
+/*   Updated: 2023/04/02 20:27:19 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <stdio.h>
+
+typedef struct s_philo
+{
+	int				index;
+	struct s_philo	*next;
+}			t_philo;
 
 #endif
