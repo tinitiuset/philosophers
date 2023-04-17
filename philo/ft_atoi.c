84468ec10933b35b859ab0c4016cf8026bf6688f
tinitiuset <tinitiuset@gmail.com>
2023-04-17 20:02:15 +0200

    Added struct fork and atoi to project.

---
+++ philo/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 19:11:41 by mvalient          #+#    #+#             */
+/*   Updated: 2023/04/17 19:12:01 by mvalient         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isdigit(int c)
+{
+	if (48 <= c && c <= 57)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	nb;
+	int	sign;
+
+	nb = 0;
+	sign = 1;
+	while ((9 <= *nptr && *nptr <= 13) || *nptr == ' ')
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		nb = nb * 10 + (*nptr - '0');
+		nptr++;
+	}
+	return (nb * sign);
+}
--- philo/philo.h
+++ philo/philo.h
@@ -6,19 +6,41 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 19:51:38 by mvalient          #+#    #+#             */
-/*   Updated: 2023/04/02 20:27:19 by mvalient         ###   ########.fr       */
+/*   Updated: 2023/04/17 19:40:21 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
 # include <stdio.h>
+# include <stdlib.h>
+
+typedef struct s_stats
+{
+	int	number_philo;
+	int	time_die;
+	int	time_eat;
+	int	time_sleep;
+	int	must_eat;
+}			t_stats;
+
+typedef struct s_fork
+{
+	int				index;
+	struct s_fork	*next;
+}			t_fork;
 
 typedef struct s_philo
 {
 	int				index;
+	pthread_t 		thread;
 	struct s_philo	*next;
 }			t_philo;
+
+int	ft_atoi(const char *nptr);
+t_philo *create_philo_list(int number_philo);
+t_fork *create_fork_list(int number_philo);
 
 #endif
