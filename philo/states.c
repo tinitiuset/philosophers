8a973036e6dd69835748835e1d2e73afce4f8f73
tinitiuset <tinitiuset@gmail.com>
2023-04-17 20:02:54 +0200

    Changed include declaration.

--- philo/states.c
+++ philo/states.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <philo.h>
+#include "philo.h"
 
 /*
  * Usage: nofity_state(philo, fork | eat | think | die);
